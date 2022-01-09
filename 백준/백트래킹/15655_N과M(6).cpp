bb90f5b650044ff4ea9d0bb740207a1cab9c712b
ChoiJaewon323 <casdfghjke@naver.com>
2022-01-10 02:33:54 +0900

    15655 15656 15657

---
+++ 백준/백트래킹/15655_N과M(6).cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+void func(int n, int m, vector<int>& seq, vector<int>& index) {
+	if (index.size() == m) {
+		for (auto i : index) {
+			cout << seq[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+	if (index.empty()) {
+		for (int i = 0; i < n; i++) {
+			index.push_back(i);
+			func(n, m, seq, index);
+			index.pop_back();
+		}
+		return;
+	}
+	for (int i = index.back() + 1; i < n; i++) {
+		index.push_back(i);
+		func(n, m, seq, index);
+		index.pop_back();
+	}
+}
+int main() {
+	int n, m;
+	vector<int> seq;
+	vector<int> index;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		int temp;
+		cin >> temp;
+		seq.push_back(temp);
+	}
+	sort(seq.begin(), seq.end());
+	func(n, m, seq, index);
+	return 0;
+}
