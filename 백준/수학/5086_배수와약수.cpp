800d965090361f807af12d55dd2720d614c05b1a
ChoiJaewon323 <casdfghjke@naver.com>
2022-01-13 23:12:29 +0900

    5086

---
+++ 백준/수학/5086_배수와약수.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int num1, num2;
+	while (true) {
+		cin >> num1 >> num2;
+		if (num1 == 0 && num2 == 0) {
+			break;
+		}
+		if (num1 < num2) {
+			if (num2 % num1 == 0) {
+				cout << "factor" << '\n';
+			}
+			else {
+				cout << "neither" << '\n';
+			}
+		}
+		else {
+			if (num1 % num2 == 0) {
+				cout << "multiple" << '\n';
+			}
+			else {
+				cout << "neither" << '\n';
+			}
+ 		}
+	}
+	return 0;
+}
