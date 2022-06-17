7fa4c7ce1472de47898d1e4570ce29f9099ed02b
choijaewon323 <casdfghjke@naver.com>
2022-06-18 02:45:44 +0900

    24480

---
+++ 백준/DFS와 BFS/24480.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+vector<list<int>> edges(200001, list<int>());
+int visited[200001];
+int seq;
+
+void dfs(int start) {
+	seq++;
+	visited[start] = seq;
+
+	for (auto i : edges[start]) {
+		if (!visited[i]) {
+			dfs(i);
+		}
+	}
+}
+
+int main() {
+	int n, m, r;
+	cin >> n >> m >> r;
+
+	for (int i = 1; i <= m; i++) {
+		int a, b;
+		cin >> a >> b;
+		edges[a].push_back(b);
+		edges[b].push_back(a);
+	}
+
+	for (int i = 1; i <= 200000; i++) {
+		edges[i].sort(greater<int>());
+	}
+
+	dfs(r);
+
+	for (int i = 1; i <= n; i++) {
+		cout << visited[i] << '\n';
+	}
+
+	return 0;
+}
