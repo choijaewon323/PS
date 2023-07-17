2e7bbd7635b9c0de8abb0d4e726e0bb6596ceb16
최재원 <46291115+choijaewon323@users.noreply.github.com>
2023-07-17 15:40:02 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1300 ms, Memory: 21012 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
@@ -1,55 +1,78 @@
-#include <iostream>
 #include <vector>
+#include <stack>
+#include <iostream>
 using namespace std;
 
 vector<int> edges;
+vector<vector<int>> transEdges;
 vector<bool> visited;
-vector<bool> done;
-int cnt;
+vector<int> tempScc;
+vector<vector<int>> scc;
+stack<int> st;
 
 void dfs(int start)
 {
-	visited[start] = true;
+	int next;
 
-	int next = edges[start];
+	visited[start] = true;
+	next = edges[start];
 
 	if (!visited[next])
 	{
 		dfs(next);
 	}
-	else if (!done[next])
+
+	st.push(start);
+}
+
+void transDfs(int start)
+{
+	int next;
+
+	visited[start] = true;
+	for (int i = 0; i < transEdges[start].size(); i++)
 	{
-		for (int i = next; i != start; i = edges[i])
+		int next = transEdges[start][i];
+
+		if (!visited[next])
 		{
-			cnt++;
+			transDfs(next);
 		}
-		cnt++;
 	}
-	done[start] = true;
+
+	tempScc.push_back(start);
 }
 
 int main()
 {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
 	int test_case;
 
 	cin >> test_case;
 
 	for (int test = 0; test < test_case; test++)
 	{
-		int n;
+		int n, answer;
 
 		cin >> n;
 
-		edges = vector<int>(n + 1, -1);
+		answer = n;
 		visited = vector<bool>(n + 1, false);
-		done = vector<bool>(n + 1, false);
+		edges = vector<int>(n + 1, -1);
+		transEdges = vector<vector<int>>(n + 1, vector<int>());
+		st = stack<int>();
 
 		for (int i = 1; i <= n; i++)
 		{
 			int temp;
 
 			cin >> temp;
+
 			edges[i] = temp;
+			transEdges[temp].push_back(i);
 		}
 
 		for (int i = 1; i <= n; i++)
@@ -60,9 +83,40 @@ int main()
 			}
 		}
 
-		cout << n - cnt << '\n';
-		cnt = 0;
-	}
+		visited = vector<bool>(n + 1, false);
 
-	return 0;
+		while (!st.empty())
+		{
+			int present = st.top();
+			st.pop();
+
+			if (!visited[present])
+			{
+				tempScc.clear();
+				transDfs(present);
+				scc.push_back(tempScc);
+			}
+		}
+
+		for (int i = 0; i < scc.size(); i++)
+		{
+			if (scc[i].size() >= 2)
+			{
+				answer -= scc[i].size();
+			}
+			else if (scc[i].size() == 1)
+			{
+				int temp = scc[i].front();
+
+				if (edges[temp] == temp)
+				{
+					answer--;
+				}
+			}
+		}
+
+		cout << answer << '\n';
+
+		scc.clear();
+	}
 }
