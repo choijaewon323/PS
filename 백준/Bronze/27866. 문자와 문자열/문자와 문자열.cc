bf82e72a7f7cf48d75fa5983f3b22da3989c4905
최재원 <46291115+choijaewon323@users.noreply.github.com>
2023-07-28 16:46:14 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string input;
+
+	cin >> input;
+
+	int i;
+
+	cin >> i;
+
+	cout << input[i - 1] << '\n';
+
+	return 0;
+}
