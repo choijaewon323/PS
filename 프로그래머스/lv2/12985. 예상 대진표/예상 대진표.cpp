f530967ef4fdf767c56d33c1f50d296cc68d69cf
최재원 <46291115+choijaewon323@users.noreply.github.com>
2023-08-22 11:57:56 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 0;
+
+    while (a != b) {
+        if (a % 2 == 1) {
+            a = a / 2 + 1;
+        }
+        else {
+            a = a / 2;
+        }
+        
+        if (b % 2 == 1) {
+            b = b / 2 + 1;
+        }
+        else {
+            b = b / 2;
+        }
+        answer++;
+    }
+    
+    return answer;
+}
