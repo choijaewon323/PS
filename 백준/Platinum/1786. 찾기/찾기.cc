96efd5d50087ab6f673b6c56279c54f9eff72416
최재원 <46291115+choijaewon323@users.noreply.github.com>
2023-12-15 14:32:43 +0900

    [Platinum V] Title: 찾기, Time: 60 ms, Memory: 9432 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.cc
+++ 백준/Platinum/1786. 찾기/찾기.cc
@@ -4,82 +4,79 @@
 
 using namespace std;
 
-vector<int> getPi(const string& P) {
-    int m = P.size();
-    vector<int> pi(m, 0);
+vector<int> getPi(string &P) {
+	int m = P.size();
+	
+	vector<int> pi(m, 0);
 
-    int begin = 1, matched = 0;
+	int begin = 1, matched = 0;
 
-    while (begin + matched < m) {
-        if (P[begin + matched] == P[matched]) {
-            ++matched;
+	while (begin + matched <= m) {
+		if (P[begin + matched] == P[matched]) {
+			matched++;
+			pi[begin + matched - 1] = matched;
+		}
+		else {
+			if (matched == 0) {
+				begin++;
+			}
+			else {
+				begin += matched - pi[matched - 1];
+				matched = pi[matched - 1];
+			}
+		}
+	}
 
-            pi[begin + matched - 1] = matched;
-        }
-        else {
-            if (matched == 0) {
-                ++begin;
-            }
-            else {
-                begin += matched - pi[matched - 1];
-                matched = pi[matched - 1];
-            }
-        }
-    }
-
-    return pi;
+	return pi;
 }
 
-vector<int> kmp(const string& T, const string& P) {
-    int n = T.size();
-    int m = P.size();
+vector<int> kmp(string &T, string &P) {
+	int n = T.size();
+	int m = P.size();
+	vector<int> result;
+	vector<int> pi = getPi(P);
 
-    vector<int> result;
-    vector<int> pi = getPi(P);
+	int begin = 0, matched = 0;
 
-    int begin = 0, matched = 0;
+	while (begin <= n - m) {
+		if (matched < m && T[begin + matched] == P[matched]) {
+			matched++;
 
-    while (begin <= n - m) {
-        if (matched < m && T[begin + matched] == P[matched]) {
-            ++matched;
+			if (matched == m) {
+				result.push_back(begin);
+			}
+		}
+		else {
+			if (matched == 0) {
+				begin++;
+			}
+			else {
+				begin += matched - pi[matched - 1];
+				matched = pi[matched - 1];
+			}
+		}
+	}
 
-            if (matched == m) {
-                result.push_back(begin);
-            }
-        }
-        else {
-            if (matched == 0) {
-                ++begin;
-            }
-            else {
-                begin += matched - pi[matched - 1];
-                matched = pi[matched - 1];
-            }
-        }
-    }
-
-    return result;
+	return result;
 }
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-    string T, P;
+	string T, P;
 
-    getline(cin, T);
+	getline(cin, T);
+	getline(cin, P);
 
-    getline(cin, P);
+	vector<int> answer = kmp(T, P);
 
-    vector<int> answers = kmp(T, P);
-    
-    cout << answers.size() << '\n';
-
-    for (int ans : answers) {
-        cout << ans + 1 << " ";
-    }
-    cout << '\n';
-
-    return 0;
+	cout << answer.size() << '\n';
+	for (int index : answer) {
+		cout << index + 1 << " ";
+	}
+	cout << '\n';
+	
+	return 0;
 }
