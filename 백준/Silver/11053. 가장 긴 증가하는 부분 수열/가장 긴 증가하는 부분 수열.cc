5266e77f7c5cadd73899bc90cca8e56f08be2815
최재원 <46291115+choijaewon323@users.noreply.github.com>
2024-01-23 15:23:26 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,30 +1,61 @@
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
-int n;
-int arr[1000];
-int cache[1001];
-int lis3(int start) {
-	int& ret = cache[start + 1];
-	if (ret != -1) {
-		return ret;
-	}
-	ret = 1;
-	for (int next = start + 1; next < n; next++) {
-		if (start == -1 || arr[start] < arr[next]) {
-			ret = max(ret, lis3(next) + 1);
-		}
-	}
-	return ret;
-}
-int main() {
-	for (int i = 0; i < 1000; i++) {
-		cache[i] = -1;
-	}
-	cin >> n;
-	for (int i = 0; i < n; i++) {
+
+#define MAX 1000
+
+int N;
+int arr[MAX];
+int dp[MAX];
+
+void input() 
+{
+	cin >> N;
+
+	for (int i = 0; i < N; i++) 
+	{
 		cin >> arr[i];
 	}
-	cout << lis3(-1) - 1 << '\n';
+}
+
+int solve() 
+{
+	for (int i = 0; i < N; i++) 
+	{
+		const int present = arr[i];
+
+		int temp = 0;
+		for (int j = 0; j < i; j++) 
+		{
+			if (arr[j] < present) 
+			{
+				temp = max(temp, dp[j]);
+			}
+		}
+
+		dp[i] = temp;
+		dp[i]++;
+	}
+
+	int answer = 0;
+	for (int i = 0; i < N; i++)
+	{
+		answer = max(answer, dp[i]);
+	}
+
+	return answer;
+}
+
+int main() 
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	input();
+
+	cout << solve() << '\n';
+
 	return 0;
 }
