c10fb28cbdd8df12d3a433ea02b6a5055439f231
최재원 <46291115+choijaewon323@users.noreply.github.com>
2024-01-27 10:47:59 +0900

    [Silver IV] Title: 큐, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	queue<int> st;
+	for (int commands = 0; commands < N; commands++)
+	{
+		string command;
+		cin >> command;
+
+		if (command == "push")
+		{
+			int x;
+			cin >> x;
+			st.push(x);
+		}
+		else if (command == "pop")
+		{
+			if (st.empty())
+			{
+				cout << -1 << '\n';
+			}
+			else
+			{
+				cout << st.front() << '\n';
+				st.pop();
+			}
+		}
+		else if (command == "size")
+		{
+			cout << st.size() << '\n';
+		}
+		else if (command == "empty")
+		{
+			cout << (int)st.empty() << '\n';
+		}
+		else if (command == "front")
+		{
+			if (st.empty())
+			{
+				cout << -1 << '\n';
+			}
+			else
+			{
+				cout << st.front() << '\n';
+			}
+		}
+		else
+		{
+			if (st.empty())
+			{
+				cout << -1 << '\n';
+			}
+			else
+			{
+				cout << st.back() << '\n';
+			}
+		}
+	}
+
+	return 0;
+}
