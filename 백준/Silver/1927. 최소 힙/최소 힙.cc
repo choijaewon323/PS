7289b144662d4fde2ab1484046eeb0c6fcc5668f
최재원 <46291115+choijaewon323@users.noreply.github.com>
2024-01-29 15:10:18 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <utility>
+#include <queue>
+
+using namespace std;
+
+int main() 
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	priority_queue<int> pq;
+	for (int i = 0; i < N; i++)
+	{
+		int x;
+		cin >> x;
+		if (x != 0)
+		{
+			pq.push(-x);
+		}
+		else
+		{
+			if (pq.empty())
+			{
+				cout << 0 << '\n';
+			}
+			else
+			{
+				cout << -(pq.top()) << '\n';
+				pq.pop();
+			}
+		}
+	}
+
+	return 0;
+}
