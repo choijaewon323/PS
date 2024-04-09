7e0878ba0bbfe3d7179f35244763b7c565191379
최재원 <46291115+choijaewon323@users.noreply.github.com>
2024-04-09 16:49:38 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 168 ms, Memory: 12200 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+#define MAX 1000000
+
+int arr[MAX];
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+
+	int answer = 0;
+	vector<int> result;
+	for (int i = 0; i < N; i++) {
+		int present = arr[i];
+
+		if (result.empty()) {
+			result.push_back(present);
+		} else {
+			int back = result.back();
+
+			if (back < present) {
+				result.push_back(present);
+			} else {
+				int index = lower_bound(result.begin(), result.end(), present) - result.begin();
+				
+				result[index] = present;
+			}
+		}
+
+		answer = max(answer, (int)result.size());
+	}
+
+	cout << answer << '\n';
+
+	return 0;
+}
